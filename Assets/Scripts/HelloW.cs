6474f5a7477200586c9a5c588f524b0ff2c2bc62
mari999p <mariia999@bk.ru>
2024-08-05 18:40:47 +0300

    Init project

---
+++ Assets/Scripts/HelloW.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    #region Unity lifecycle
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Debug.Log("Start");
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Debug.Log("Update");
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/MagicNumbers.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class MagicNumbers : MonoBehaviour
+{
+    #region Variables
+
+    [SerializeField] private int _max = 1000;
+    [SerializeField] private int _min = 500;
+
+    private int _guess;
+
+    #endregion
+
+    #region Unity lifecycle
+
+    private void Start()
+    {
+        Debug.Log("Привет в Magic Numbers. Загадай число от {_min} до {_max}");
+
+        CalculateGuessAndLong();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            _guess = _max;
+            CalculateGuessAndLong();
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            _min = _guess;
+            CalculateGuessAndLong();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log($"УРА! Твое число отгадано и равно {_guess}!");
+        }
+    }
+
+    #endregion
+
+    #region Private methods
+
+    private void CalculateGuessAndLong()
+    {
+        _guess = (_max + _min) / 2;
+        Debug.Log($"Твое число равно {_guess}?");
+    }
+
+    #endregion
+}
